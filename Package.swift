b190280365aaeb570d65a8feead6c98881c28fe4
farfromrefuge <dev@akylas.fr>
2025-10-15 17:56:32 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CartoMobileSDK",
-            url: "https://github.com/Akylas/mobile-sdk/releases/download/5.0.0/carto-mobile-sdk-ios-metal-5.0.0.zip",
+            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.0.0/carto-mobile-sdk-ios-metal-5.0.0.zip",
             checksum: "92e22c9924a99d7b6e7aee003fbb0734f1b6f82bcef0f756cae1403284f5fabf"
         )
     ]
