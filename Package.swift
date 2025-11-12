22a9c09201ec8cd445435b48f58b0ce7f71725f4
Martin Guillon <dev@akylas.fr>
2025-11-12 09:24:45 +0000

    Automated update Package.swift from workflow for new release

--- Package.swift
+++ Package.swift
@@ -17,9 +17,19 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "CartoMobileSDK",
-            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.0.0/carto-mobile-sdk-ios-metal-5.0.0.zip",
-            checksum: "7559a9476a3e559626dd85d94c1999d1a32b1a0ee2fe660d8754cc4461d428e8"
-        )
+            name: "CartoMobileSDK-valhalla",
+            url: "https://github.com/Akylas/mobile-sdk/releases/download/5.0.1/carto-mobile-sdk-ios-5.0.1-valhalla.zip",
+            checksum: "e8547ba2d20a4eb2ee63ba89f23e21d35d8c74645277476ab17ac5613ce9aeb4"
+        ),
+        .binaryTarget(
+            name: "CartoMobileSDK-core",
+            url: "https://github.com/Akylas/mobile-sdk/releases/download/5.0.1/carto-mobile-sdk-ios-5.0.1-core.zip",
+            checksum: "01ac83a6d5f176f9f1838c8a66f8e31fb25b84d99de12d5006d914a45fb3edca"
+        ),
+        .binaryTarget(
+            name: "CartoMobileSDK-lite",
+            url: "https://github.com/Akylas/mobile-sdk/releases/download/5.0.1/carto-mobile-sdk-ios-5.0.1-lite.zip",
+            checksum: "dadde1e903e06b98f0a5e774db8196e73ef15d6b696cfc6d69fea1edeef85771"
+        ),
     ]
 )
