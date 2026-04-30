7ec2a149c06d1471ed241366a6847e16cf2e9eaf
Martin Guillon <dev@akylas.fr>
2026-04-30 20:48:15 +0000

    Automated update Package.swift from workflow for new release

--- Package.swift
+++ Package.swift
@@ -18,23 +18,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CartoMobileSDK",
-            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.2.1/carto-mobile-sdk-ios-5.2.1-full.zip",
-            checksum: "869ff039db4a867bb66f64d7649c104929d33347b72951197945c8518075c138"
+            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.2.2/carto-mobile-sdk-ios-5.2.2-full.zip",
+            checksum: "f776d5e27b624e1d641c3d2ea1db933951fca3ec76b87f1ca84292a55ded8b11"
         ),
         .binaryTarget(
             name: "CartoMobileSDK-core",
-            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.2.1/carto-mobile-sdk-ios-5.2.1-core.zip",
-            checksum: "201ff8c33c48bf0c41f38721890a67e92464001e40b54bfdd8931a10607b23f2"
+            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.2.2/carto-mobile-sdk-ios-5.2.2-core.zip",
+            checksum: "c8f3825aedd7884be409191dadf8edc701aed46edaf2ba492a89dc0e628359d3"
         ),
         .binaryTarget(
             name: "CartoMobileSDK-lite",
-            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.2.1/carto-mobile-sdk-ios-5.2.1-lite.zip",
-            checksum: "568314460911a9dd59aab04219f05c4a1368c88df04b96c4448481dafe77e247"
+            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.2.2/carto-mobile-sdk-ios-5.2.2-lite.zip",
+            checksum: "55899b3599d9d6b17159cb513d80516b725b58116da2f6eacd8d9db9ae896b8e"
         ),
         .binaryTarget(
             name: "ValhallaRouting",
-            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.2.1/carto-routing-ios-5.2.1.zip",
-            checksum: "cbd6d5b2be3be3b578caa777b5fb03b0f0e8a8c2158a3b2980fa594332d32924"
+            url: "https://github.com/Akylas/mobile-sdk/releases/download/v5.2.2/carto-routing-ios-5.2.2.zip",
+            checksum: "d7d6b97be721fe488e9af066702f4cca09e5a23b2ad552eb398054bd576c9375"
         ),
     ]
 )
